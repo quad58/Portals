36a3e323837a720e5e779fb803d2df01385dab09
Quad <devtravel.main@gmail.com>
2023-06-12 00:54:36 +0300

    Shadows through portals

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -4,23 +4,28 @@ using VolumetricLighting;
 
 namespace Portals
 {
-    [RequireComponent(typeof(MeshRenderer))]
     public class Portal : MonoBehaviour
     {
         public Portal Partner;
         [HideInInspector] public Camera Camera;
 
-        private MeshRenderer MeshRenderer;
+        public MeshRenderer MeshRenderer;
 
         private Camera MainCamera;
 
-        public List<Light> Lights = new List<Light>();
-        private Dictionary<int, Light> LightsBeyondThePortal = new Dictionary<int, Light>();
+        public List<Light> LightsToCopy = new List<Light>();
+        private Dictionary<int, VolumetricLight> VolumetricLightsToCopy = new Dictionary<int, VolumetricLight>();
+        private Dictionary<int, Light> CopiedLights = new Dictionary<int, Light>();
+        private Dictionary<int, VolumetricLight> CopiedVolumecticLights = new Dictionary<int, VolumetricLight>();
+
+        public List<GameObject> ObjectsToCopy = new List<GameObject>();
+        private Dictionary<int, GameObject> CopiedObjects = new Dictionary<int, GameObject>();
+        private Dictionary<int, MeshRenderer> CopiedObjectsMeshRenderers = new Dictionary<int, MeshRenderer>();
+
+        public bool DebugMode;
 
         private void Awake()
         {
-            MeshRenderer = GetComponent<MeshRenderer>();
-
             MainCamera = Camera.main;
         }
 
@@ -37,6 +42,12 @@ namespace Portals
                 UpdateCamera();
             }
             UpdateLights();
+            UpdateCopiedObjects();
+
+            if (Input.GetKeyDown(KeyCode.F3))
+            {
+                DebugMode = !DebugMode;
+            }
         }
 
         private void OnTriggerStay(Collider other)
@@ -76,54 +87,117 @@ namespace Portals
             Camera.backgroundColor = MainCamera.backgroundColor;
             Camera.fieldOfView = MainCamera.fieldOfView;
 
-            Vector3 relativePosition = transform.InverseTransformPoint(MainCamera.transform.position);
-            relativePosition = Quaternion.Euler(0, 180, 0) * relativePosition;
-            Camera.transform.position = Partner.transform.TransformPoint(relativePosition);
+            Vector3 viewerPosition = Partner.transform.worldToLocalMatrix.MultiplyPoint3x4(MainCamera.transform.position);
+            Camera.transform.localPosition = new Vector3(-viewerPosition.x, viewerPosition.y, -viewerPosition.z);
 
             Camera.transform.rotation = transform.rotation * Quaternion.Inverse(Partner.transform.rotation * Quaternion.Euler(0, 180, 0)) * MainCamera.transform.rotation;
         }
 
         public void UpdateLights()
         {
-            for (int i = 0; i < Lights.Count; i++)
+            for (int i = 0; i < LightsToCopy.Count; i++)
             {
-                if (LightsBeyondThePortal.ContainsKey(i))
+                if (CopiedLights.ContainsKey(i))
                 {
-                    if (LightsBeyondThePortal[i] == null)
+                    if (CopiedLights[i] == null)
                     {
-                        LightsBeyondThePortal[i] = Instantiate(Lights[i].gameObject).GetComponent<Light>();
-                        LightsBeyondThePortal[i].name = gameObject.name + " " + LightsBeyondThePortal[i].name;
-                        LightsBeyondThePortal[i].transform.SetParent(Partner.transform);
+                        CopiedLights[i] = Instantiate(LightsToCopy[i].gameObject).GetComponent<Light>();
+                        CopiedLights[i].name = gameObject.name + " " + CopiedLights[i].name;
+                        CopiedLights[i].transform.SetParent(Partner.transform);
                     }
                     else
                     {
-                        Vector3 relativePosition = transform.InverseTransformPoint(Lights[i].transform.position);
-                        relativePosition = Quaternion.Euler(0, 180, 0) * relativePosition;
-                        LightsBeyondThePortal[i].transform.position = Partner.transform.TransformPoint(relativePosition);
+                        Vector3 lightPosition = transform.worldToLocalMatrix.MultiplyPoint3x4(LightsToCopy[i].transform.position);
+                        CopiedLights[i].transform.localPosition = new Vector3(-lightPosition.x, lightPosition.y, -lightPosition.z);
 
-                        LightsBeyondThePortal[i].transform.rotation = transform.rotation * Quaternion.Inverse(Partner.transform.rotation * Quaternion.Euler(0, 180, 0)) * Lights[i].transform.rotation;
+                        CopiedLights[i].transform.rotation = transform.rotation * Quaternion.Inverse(Partner.transform.rotation * Quaternion.Euler(0, 180, 0)) * LightsToCopy[i].transform.rotation;
 
-						LightsBeyondThePortal[i].enabled = Lights[i].enabled;
-                        LightsBeyondThePortal[i].type = Lights[i].type;
-                        LightsBeyondThePortal[i].range = Lights[i].range;
-                        LightsBeyondThePortal[i].spotAngle = Lights[i].spotAngle;
-                        LightsBeyondThePortal[i].color = Lights[i].color;
-                        LightsBeyondThePortal[i].intensity = Lights[i].intensity;
-                        LightsBeyondThePortal[i].shadows = Lights[i].shadows;
+						CopiedLights[i].enabled = LightsToCopy[i].enabled;
+                        CopiedLights[i].type = LightsToCopy[i].type;
+                        CopiedLights[i].range = LightsToCopy[i].range;
+                        CopiedLights[i].spotAngle = LightsToCopy[i].spotAngle;
+                        CopiedLights[i].color = LightsToCopy[i].color;
+                        CopiedLights[i].intensity = LightsToCopy[i].intensity;
+                        CopiedLights[i].shadows = LightsToCopy[i].shadows;
 
-                        VolumetricLight volumetricLightOriginal = Lights[i].GetComponent<VolumetricLight>();
-                        VolumetricLight volumetricLightBeyondThePortal = LightsBeyondThePortal[i].GetComponent<VolumetricLight>();
-                        volumetricLightBeyondThePortal.enabled = volumetricLightOriginal.enabled;
-                        volumetricLightBeyondThePortal.MaxRayLength = volumetricLightOriginal.MaxRayLength;
-                        volumetricLightBeyondThePortal.ScatteringCoef = volumetricLightOriginal.ScatteringCoef;
-                        volumetricLightBeyondThePortal.ExtinctionCoef = volumetricLightOriginal.ExtinctionCoef;
-                        volumetricLightBeyondThePortal.SkyboxExtinctionCoef = volumetricLightOriginal.SkyboxExtinctionCoef;
-                        volumetricLightBeyondThePortal.MieG = volumetricLightOriginal.MieG;
+                        if (VolumetricLightsToCopy.ContainsKey(i))
+                        {
+                            if (VolumetricLightsToCopy[i] != null)
+                            {
+                                if (CopiedVolumecticLights.ContainsKey(i))
+                                {
+                                    CopiedVolumecticLights[i].enabled = VolumetricLightsToCopy[i].enabled;
+                                    CopiedVolumecticLights[i].MaxRayLength = VolumetricLightsToCopy[i].MaxRayLength;
+                                    CopiedVolumecticLights[i].ScatteringCoef = VolumetricLightsToCopy[i].ScatteringCoef;
+                                    CopiedVolumecticLights[i].ExtinctionCoef = VolumetricLightsToCopy[i].ExtinctionCoef;
+                                    CopiedVolumecticLights[i].SkyboxExtinctionCoef = VolumetricLightsToCopy[i].SkyboxExtinctionCoef;
+                                    CopiedVolumecticLights[i].MieG = VolumetricLightsToCopy[i].MieG;
+                                }
+                                else
+                                {
+                                    CopiedVolumecticLights.Add(i, CopiedLights[i].GetComponent<VolumetricLight>());
+                                }
+                            }
+                        }
+                        else
+                        {
+                            VolumetricLightsToCopy.Add(i, LightsToCopy[i].GetComponent<VolumetricLight>());
+                        }
                     }
                 }
                 else
                 {
-                    LightsBeyondThePortal.Add(i, null);
+                    CopiedLights.Add(i, null);
+                }
+            }
+        }
+
+        public void UpdateCopiedObjects()
+        {
+            for (int i = 0; i < ObjectsToCopy.Count; i++)
+            {
+                if (CopiedObjects.ContainsKey(i))
+                {
+                    if (CopiedObjects[i] == null)
+                    {
+                        CopiedObjects[i] = Instantiate(ObjectsToCopy[i].gameObject);
+                        CopiedObjects[i].name = gameObject.name + " " + CopiedObjects[i].name;
+                        CopiedObjects[i].transform.SetParent(Partner.transform);
+
+                        CopiedObjectsMeshRenderers.Add(i, CopiedObjects[i].GetComponent<MeshRenderer>());
+                    }
+                    else
+                    {
+                        Vector3 lightPosition = transform.worldToLocalMatrix.MultiplyPoint3x4(ObjectsToCopy[i].transform.position);
+                        CopiedObjects[i].transform.localPosition = new Vector3(-lightPosition.x, lightPosition.y, -lightPosition.z);
+
+                        CopiedObjects[i].transform.rotation = transform.rotation * Quaternion.Inverse(Partner.transform.rotation * Quaternion.Euler(0, 180, 0)) * ObjectsToCopy[i].transform.rotation;
+
+                        if (DebugMode)
+                        {
+                            if (CopiedObjectsMeshRenderers.ContainsKey(i))
+                            {
+                                if (CopiedObjectsMeshRenderers[i] != null)
+                                {
+                                    CopiedObjectsMeshRenderers[i].shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if (CopiedObjectsMeshRenderers.ContainsKey(i))
+                            {
+                                if (CopiedObjectsMeshRenderers[i] != null)
+                                {
+                                    CopiedObjectsMeshRenderers[i].shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    CopiedObjects.Add(i, null);
                 }
             }
         }
